5c79d2cf12de44eabcb005dfe9e389d5ac8b6b3c
office-hue <office@sharity.hu>
2025-10-05 17:10:02 +0200

    repo: canonical WP tree – plugins/mu-plugins rendezve, 3rd-party bundlék kizárva

--- wp-content/plugins/advanced-custom-fields/hellopack-client/inc/admin/view/callback/section/oauth.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * OAuth section
- *
- * @package HelloPack_Client
- * @since 2.0.0
- */
--- wp-content/plugins/advanced-custom-fields/hellopack-client/inc/legacy/modules/module-plugin-woocommerce-appointments.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
-
-if ( hp_is_plugin_activated( 'woocommerce-appointments', 'woocommerce-appointments.php' ) ) {
-	HP_check_options( 'bizz_woocommerce_appointments_license_active', 'valid' );
-}
